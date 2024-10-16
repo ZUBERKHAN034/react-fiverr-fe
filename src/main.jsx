e034bfc077e79fe2e73ff746371c6529f5880072
Zuber Khan <zuberkhan034@gmail.com>
2024-10-16 12:36:22 +0530

    ✨ added userback.io feedback form

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { UserbackProvider } from '@userback/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <UserbackProvider token={import.meta.env.VITE_USERBACK_API_TOKEN_KEY}>
+      <App />
+    </UserbackProvider>
   </React.StrictMode>,
 )
